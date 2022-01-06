853959e0c56f31ba46eb47f8e489892a20207b83
Unknown0wl <88095098+Unknown0wl@users.noreply.github.com>
2022-01-06 14:46:29 +0100

    smoth navbar mobile

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-new fullpage('#fullpage', {
+x = new fullpage('#fullpage', {
     navigation: true,
     navigationPosition: 'right',
     showActiveTooltip: true,
+    anchors: ['#s1', '#s2', '#s3', '#s4']
 });
+
 
 
 let menu = document.getElementById("tend");
 function navbar() {
     menu.setAttribute("class", "on");
-    menu.classList.contains('on');
+    let d = menu.classList.contains("on");
+
+    if(d == true){
+        x.navigation = false;
+    }else{
+        x.navigation = true;
+    }
 }
+ 
 
 function exit(){
+    menu.removeAttribute("class", "on")
     menu.setAttribute("class", "off");
-    menu.classList.contains('off');
 }
+
+function exitp(){
+    menu.removeAttribute("class", "on")
+    menu.setAttribute("class", "off");
+    
+}
+
